53ff684cf41ed86f144ed2f4658631ccb08fb173
yuvalSade457 <sadeyuv@post.bgu.ac.il>
2026-03-10 10:34:46 +0200

    added base route '/'

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,7 @@ app.get('/api/hello', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('The server is up and running!');
+});
