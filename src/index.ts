4de52fd4b7ac33e5fb6d5be5e9a526c62919ef55
yuvalSade457 <sadeyuv@post.bgu.ac.il>
2026-03-10 10:45:07 +0200

    changing app.get ('/') place

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('The server is up and running!');
+});
+
 // Define a basic route
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the backend!' });
@@ -11,8 +15,4 @@ app.get('/api/hello', (req: Request, res: Response) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('The server is up and running!');
 });
